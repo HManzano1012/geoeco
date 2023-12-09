e4dc76b4ce780297d6292c59e9f18ab34f781a46
Harold Manzano <hmanzano1012@gmail.com>
2023-12-08 23:26:06 -0600

    fist commit

--- app/Views/index.php
+++ app/Views/index.php
@@ -38,7 +38,7 @@
                     <a class="nav-link" href="#portfolio">Sucursales</a>
                 </li>
                 <li class="nav-item mx-2">
-                    <a class="nav-link" href="#about">Sobre nuestro proyecto</a>
+                    <a class="nav-link" href="#info">Informacion </a>
                 </li>
                 <li class="nav-item mx-2">
                     <a class="nav-link" href="#contact">Solicitud de sucursal</a>
@@ -49,47 +49,19 @@
     </div>
 </nav>
 
-<header class="masthead bg-primary text-white text-center">
-    <div class="container d-flex align-items-center flex-column">
+<div class="w-100">
+<img src="https://www.digitaltrends.com/wp-content/uploads/2020/02/gettyimages-494832555-scaled.jpg?fit=2560%2C1706&p=1" style="height:100vh;width:100%!important;object-fit:cover"/>
 
-        <header class="masthead bg-primary text-white text-center">
-            <video autoplay loop muted playsinline poster="assets/videos/fondo.mp4" style="height: 540px; width: 100vw; object-fit: cover; position: relative; top: -366px;">
-                <source src="assets/videos/fondo.mp4" type="video/mp4">
-            </video>
-            <div class="container d-flex align-items-center flex-column">
-
-            </div>
-        </header>
-</header>
+</div>
 
 <section class="page-section portfolio" id="portfolio">
     <div class="container">
-
-        <h4 class="page-section-heading text-center text-uppercase text-secondary mb-0">Sucursales</h4>
-        <div class="divider-custom">
-            <div class="divider-custom-line"></div>
-        </div>
+        <h4 class=" text-center text-uppercase text-secondary mb-0">Sucursales aptas para manejar los desechos electronicos</h4>
         <div class="row justify-content-center">
-<div class="col-md-12 m-5">
-  <div class="row">
-    <!--Filtros <select id="zone_filter" class="form-control col-3">
-<option value="">Selecciona una zona</option>
-<?php foreach ($zones as $zone) :?>
-<option value="<?php echo $zone->ID?>"><?php echo $zone->ZoneName?></option>
-<?php endforeach; ?>
-</select>
-     <select id="department_filter" class="form-control col-3">
-<option value="">Selecciona un departmento</option>
-</select>
-     <select id="municipio_filter" class="form-control col-3">
-<option value="">Selecciona un municipio</option>
-</select>-->
-  </div>
-</div>
-  
+
 
 <table id='places_table'>
-<thead><tr><th>Nombre</th><th>Direccion</th><th>Municipio</th><th>Descripcion</th><th></th></tr></thead>
+<thead><tr><th>Nombre</th><th>Direccion</th><th>Municipio</th><th></th></tr></thead>
 <tbody>
 <?php foreach ($places as $place) :?>
 
@@ -97,8 +69,8 @@
 <td><?php echo $place->name?></td>
 <td><?php echo $place->address?></td>
 <td><?php echo $place->municipio?></td>
-<td><?php echo $place->description?></td>
-<td><button class="btn btn-sm btn-secondary">Detalles</button> <a class="btn btn-sm btn-primary" href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $place->lat?>,<?php echo $place->lon?>" target="_blank">Ver en Google Maps</a></td>
+<td><button class="btn btn-sm btn-secondary details-btn"  data-bs-toggle="modal" data-bs-target="#place_details" data-id="<?php echo $place->id?>">Detalles</button>
+ <a class="btn btn-sm btn-primary" href="https://www.google.com/maps/dir/?api=1&destination=<?php echo $place->lat?>,<?php echo $place->lon?>" target="_blank">Ver en Maps</a></td>
 </tr>
 
 <?php endforeach; ?>
@@ -107,47 +79,45 @@
         </div>
     </div>
 </section>
-<section class="page-section bg-primary text-white mb-0" id="about">
+
+<section class="page-section" id="info">
     <div class="container">
-        <h2 class="page-section-heading text-center text-uppercase text-white">QUE PROPONE NUESTRO PROYECTO</h2>
-        <div class="divider-custom divider-light">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
-        </div>
-        <div class="">
-            <div class="lead">
-                <p class="">El proyecto propone un sistema de programación para la gestión eficiente de residuos electrónicos, abordando desde la recopilación hasta la clasificación y reutilización. Se destaca la importancia de la concientización y se plantea la
-                    implementación de un software que facilite la localización de puntos de reciclaje y promueva la educación sobre prácticas adecuadas de disposición electrónica.</p>
-                <p class="">
-                    Se identifican ventajas significativas como la mayor eficiencia en la recolección y clasificación, la reducción de costos operativos y el impacto ambiental minimizado. Sin embargo, se reconocen desafíos como la integración con sistemas existentes, la
-                    seguridad de datos y la capacitación del personal.
+        <h4 class="text-center text-uppercase text-secondary mb-2">Informacion sobre los desechos electronicos</h4>
+        <div class="row justify-content-center my-2">
+            <div class="col-lg-8 col-xl-7 my-5">
+                <p class="lead mb-0">
+                    Los desechos electrónicos son aquellos que se generan por el desuso de aparatos electrónicos y eléctricos, como computadoras, celulares, televisores, electrodomésticos, entre otros. Estos desechos contienen sustancias tóxicas que pueden contaminar el suelo, el agua y el aire, afectando la salud de las personas y el medio ambiente.
+                </p>
+            </div>
+            <div class="col-lg-8 col-xl-7 my-5">
+                <p class="lead mb-0">
+                    En El Salvador, la Ley de Gestión Integral de Residuos Sólidos (LGIRS) establece que los desechos electrónicos deben ser tratados de manera especial, ya que contienen sustancias tóxicas que pueden contaminar el suelo, el agua y el aire, afectando la salud de las personas y el medio ambiente.
                 </p>
             </div>
         </div>
-    </div>
-</section>
-<section class="page-section bg-primary text-white mb-0" id="about">
-    <div class="container">
-<h2 class="page-section-heading text-center text-uppercase text-white">QUE PROPONE NUESTRO PROYECTO</h2>
-<div class="divider-custom divider-light">
-    <div class="divider-custom-line"></div>
-    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-    <div class="divider-custom-line"></div>
-</div>
-        <div class="">
-            <div class="lead">
-                <p class="">El proyecto propone un sistema de programación para la gestión eficiente de residuos electrónicos, abordando desde la recopilación hasta la clasificación y reutilización. Se destaca la importancia de la concientización y se plantea la
-                    implementación de un software que facilite la localización de puntos de reciclaje y promueva la educación sobre prácticas adecuadas de disposición electrónica.</p>
-                <p class="">
-                    Se identifican ventajas significativas como la mayor eficiencia en la recolección y clasificación, la reducción de costos operativos y el impacto ambiental minimizado. Sin embargo, se reconocen desafíos como la integración con sistemas existentes, la
-                    seguridad de datos y la capacitación del personal.
+        <div class="row justify-content-center my-2">
+            <div class="col-lg-8 col-xl-7 my-5">
+                <p class="lead mb-0">
+
+                Antes de desechar un dispositivo electrónico, considera si puede ser reacondicionado o reutilizado, algunas organizaciones aceptan donaciones de equipos electrónicos para repararlos y redistribuirlos a personas que los necesiten.
+                </p>
+            </div>
+        </div>
+        <h4 class="text-center text-uppercase text-secondary mb-2">Centros especializados</h4>
+        <div class="row justify-content-center my-2">
+            <div class="col-lg-8 col-xl-7 my-5">
+                <p class="lead mb-0">
+
+                 Busca centros de reciclaje especializados en electrónicos, estos lugares están equipados para manejar adecuadamente la destrucción de datos y la separación de componentes para su reciclaje. Programas de devolución del fabricante: Algunas compañías tienen programas de devolución de productos electrónicos antiguos, puedes devolver tus dispositivos obsoletos a los fabricantes para que los reciclen de manera adecuada.
+
+
                 </p>
             </div>
         </div>
 
-</div>
+    </div>
 </section>
+
 <section class="page-section" id="contact">
     <div class="container">
         <h4 class="text-center text-uppercase text-secondary mb-2">Eres una sucursal apta para el manejo de desechos eletronicos?</h4>
@@ -209,7 +179,7 @@
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
             </div>
             <div class="col-lg-4">
-                <h4 class="text-uppercase mb-4">Programación IV</h4>
+                <h4 class="text-uppercase mb-4"></h4>
                 <p class="lead mb-0">
                     Creada con la intención de informar a la población
                 </p>
@@ -221,40 +191,32 @@
 <div class="copyright py-4 text-center text-white">
     <div class="container"><small>Copyright &copy; UNAB 2023</small></div>
 </div>
-  <div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" aria-labelledby="portfolioModal4" aria-hidden="true">
-          <div class="modal-dialog modal-xl">index.php
-              <div class="modal-content">
-                  <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                  <div class="modal-body text-center pb-5">
-                      <div class="container">
-                          <div class="row justify-content-center">
-                              <div class="col-lg-8">
-                                  <!-- Portfolio Modal - Title-->
-                                  <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Controller</h2>
-                                  <!-- Icon Divider-->
-                                  <div class="divider-custom">
-                                      <div class="divider-custom-line"></div>
-                                      <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                      <div class="divider-custom-line"></div>
-                                  </div>
-                                  <!-- Portfolio Modal - Image-->
-                                  <img class="img-fluid rounded mb-5" src="assets/img/portfolio/game.png" alt="..." />
-                                  <!-- Portfolio Modal - Text-->
-                                  <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <button class="btn btn-primary" data-bs-dismiss="modal">
-                                      <i class="fas fa-xmark fa-fw"></i>
-                                      Close Window
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-
-
+ <div class="portfolio-modal modal fade" id="place_details" tabindex="-1" aria-labelledby="portfolioModal5" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                    <div class="modal-body text-center pb-5">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="row">
+                                  <div class="col-md-6 col-lg-6 col-sm-12 m-0 d-inline-block">
+                                    <div id="map_details" class="mb-3 " style="height:500px"> </div>
+                                 </div>
+                                    <div class="col-md-6 col-lg-6 col-sm-12 m-0 d-inline-block">
+                                        <h1 id="place_name"></h1>
+<strong class="text-bold">Descripcion</strong>
+                                        <p class="mb-4" id="place_description"></p>
+<strong class="text-bold">Direccion</strong>
+                                        <p class="mb-4" id="place_direccion"></p>
+ <a class="btn btn-primary" href="" id="open_maps" target="_blank">Ver en Maps</a></td>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="js/scripts.js"></script>
@@ -267,37 +229,37 @@
 
 $(document).ready(function(){
 
-  let lat = 13.6956365
-  let lon = -88.9483076
-  $("#latitude").val(lat)
-  $("#longitude").val(lon)
-  var locationpicker = $('#map').locationpicker({
-    location: {
-      latitude: lat, 
-      longitude: lon
+    let lat = 13.6956365
+        let lon = -88.9483076
+        $("#latitude").val(lat)
+        $("#longitude").val(lon)
+        var locationpicker = $('#map').locationpicker({
+        location: {
+        latitude: lat, 
+            longitude: lon
     },   
     radius: 0,
     zoom: 9,
     onchanged: function(currentLocation, radius, isMarkerDropped) {
-      $("#latitude").val(currentLocation.latitude)
-      $("#longitude").val(currentLocation.longitude)
+        $("#latitude").val(currentLocation.latitude)
+            $("#longitude").val(currentLocation.longitude)
     },
     onclicked: function(currentLocation, radius, isMarkerDropped) {
-      alert("test")
-      $("#latitude").val(currentLocation.latitude)
-      $("#longitude").val(currentLocation.longitude)
+        alert("test")
+            $("#latitude").val(currentLocation.latitude)
+            $("#longitude").val(currentLocation.longitude)
     },
     enableAutocomplete: true,
     enableAutocompleteBlur: true,
     autocompleteOptions: {
-      types: ['(cities)'],
-      componentRestrictions: {country: 'sv'}
+    types: ['(cities)'],
+        componentRestrictions: {country: 'sv'}
     },
   });
 
   let tables_datatable =  $("#places_table").DataTable({
-    "language": {
-      "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
+  "language": {
+  "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
     },
     searching: false,
     paging: true,
@@ -311,32 +273,61 @@ $(document).ready(function(){
   $("#zone_filter").select2()
   $("#department_filter").select2()
   $("#municipio_filter").select2()
+  var data = <?php echo json_encode($places)?>;
 
   $('#add_suc').submit(function(e){
-    e.preventDefault()
-    let data = $(this).serialize()
-    let hostURL = $("#base_url").val()
-    $.ajax({
-      method:"POST",
-      url:hostURL + "api/savesuc",
-      data: data,
-      dataType:"json",
-      success: function(response){
-        $('#add_suc').trigger("reset")
-        $('#municipio').trigger("change")
+      e.preventDefault()
+          let data = $(this).serialize()
+          let hostURL = $("#base_url").val()
+          $.ajax({
+          method:"POST",
+              url:hostURL + "api/savesuc",
+              data: data,
+              dataType:"json",
+              success: function(response){
+                  $('#add_suc').trigger("reset")
+                      $('#municipio').trigger("change")
 
-        Swal.fire({
-          text: "Hemos recibido tu solicitud, te notificaremos cuando se haya revisado",
-          icon: "success"
+                      Swal.fire({
+                      text: "Hemos recibido tu solicitud, te notificaremos cuando se haya revisado",
+                          icon: "success"
+
         });
       }
     })
   })
 
+  $(".details-btn").click(function(){
+        let id = $(this).data("id")
+        let place = data.find(x => x.id == id)
+        let lat = place.lat
+        let lon = place.lon
+        
+console.log(place)
+        $("#place_name").html(place.name)
+        $("#place_description").html(place.description)
+$("#place_direccion").html(place.address)
+$("#open_maps").attr("href","https://www.google.com/maps/dir/?api=1&destination="+place.lat+","+place.lon)
+
+        
+        
+        
+
+      var locationpicker_details = $("#map_details").locationpicker({
+      location: {
+      latitude: lat, 
+          longitude: lon
+    },   
+    radius: 0,
+    zoom: 18,
+    enableAutocomplete: true,
+    enableAutocompleteBlur: true,
+  });
+
+  })
+
 })
 
 </script>
-
-
 </body>
 </html>
